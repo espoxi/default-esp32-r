ab9f87aee155e0627e5d167118432fa15bc60056
HannesH <33062605+HannesGitH@users.noreply.github.com>
2022-11-16 15:49:09 +0100

    yessssss

---
+++ export-esp.sh
@@ -0,0 +1,5 @@
+export LIBCLANG_PATH="/Users/hanneshattenbach/.espressif/tools/xtensa-esp32-elf-clang/esp-15.0.0-20221014-aarch64-apple-darwin/esp-clang/lib"
+export PATH=/Users/hanneshattenbach/.espressif/tools/xtensa-esp32s2-elf/esp-2021r2-patch5-8_4_0/xtensa-esp32s2-elf/bin:$PATH
+export PATH=/Users/hanneshattenbach/.espressif/tools/xtensa-esp32-elf/esp-2021r2-patch5-8_4_0/xtensa-esp32-elf/bin:$PATH
+export PATH=/Users/hanneshattenbach/.espressif/tools/xtensa-esp32s3-elf/esp-2021r2-patch5-8_4_0/xtensa-esp32s3-elf/bin:$PATH
+export PATH=/Users/hanneshattenbach/.espressif/tools/riscv32-esp-elf/esp-2021r2-patch5-8_4_0/riscv32-esp-elf/bin:$PATH
