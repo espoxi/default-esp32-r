3a43362e3b33485c5c579894275b454150257c7a
HannesH <33062605+HannesGitH@users.noreply.github.com>
2022-11-18 19:03:14 +0100

    yuhuu, hab die api verstanden

--- src/connection.rs
+++ src/connection.rs
@@ -25,39 +25,53 @@ impl<'a> Connection<'a> {
     pub(crate) fn start_server() -> anyhow::Result<Self> {
         esp_idf_sys::link_patches();
 
-
         // let sys_loop_stack = Arc::new(EspSysLoopStack::new()?);
 
-        let conn = Connection{wifi: match wifi::wifi(CONFIG.wifi_ssid, CONFIG.wifi_psk){
-            Ok(wifi) => Some(wifi),
-            Err(e) => {
-                println!("Failed to connect to WiFi: {:?}", e);
-                None
-            }
-        }};
+        let conn = Connection {
+            wifi: match wifi::wifi(CONFIG.wifi_ssid, CONFIG.wifi_psk) {
+                Ok(wifi) => Some(wifi),
+                Err(e) => {
+                    println!("Failed to connect to WiFi: {:?}", e);
+                    None
+                }
+            },
+        };
 
         // let mut temp_sensor = BoardTempSensor::new_taking_peripherals();
 
         let server_config = Configuration::default();
         let mut server = EspHttpServer::new(&server_config)?;
         //FIXME: make this work
-        server.fn_handler("/", Method::Get, |request| {
-            let html = Self::index_html();
-            println!("someone requested the index page");
-            request
-                .into_response(200, Some(html.as_str()), &[])
-                .unwrap(); //or(anyhow::bail!("Failed to create response"));
-            Ok(())
-        })?;
+        let x = server
+            .fn_handler("/", Method::Get, |request| {
+                let html = Self::index_html();
+                println!("someone requested the index page");
+                request
+                    .into_response(
+                        200,
+                        Some("zueckrali"),//Some(html.as_str()),
+                        &[
+                            // ("content-type", "text/html"),
+                            // ("content-length", format!("{}", html.len()).as_str()),
+                        ],
+                    )
+                    .unwrap().connection().write(html.as_bytes()).unwrap(); //or(anyhow::bail!("Failed to create response"));
+                Ok(())
+            })?
+            .fn_handler("/cool", Method::Get, move |r| {
+                r.into_ok_response().unwrap();
+                Ok(())
+            })?;
 
         println!("server awaiting connection");
-
 
         // // prevent program from exiting
         //TODO: remove this if possible, or start a new thread with stuff
         loop {
             // let current_temperature = temp_sensor.read_owning_peripherals();
             // println!("board temperature: {:.2}", current_temperature);
+            // println!("");
+            // x.;
             sleep(Duration::from_millis(1000));
         }
         Ok(conn)
@@ -66,17 +80,17 @@ impl<'a> Connection<'a> {
     fn templated(content: impl AsRef<str>) -> String {
         format!(
             r#"
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>esp-rs web server</title>
-    </head>
-    <body>
-        {}
-    </body>
-</html>
-"#,
+            <!DOCTYPE html>
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                    <title>esp-rs web server</title>
+                </head>
+                <body>
+                    {}
+                </body>
+            </html>
+            "#,
             content.as_ref()
         )
     }
