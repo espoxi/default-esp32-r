d47e35310363b03b5bd2ecfc27a2b3965c038c79
HannesH <33062605+HannesGitH@users.noreply.github.com>
2022-11-18 10:40:23 +0100

    blinky

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,14 @@ fn main() {
     esp_idf_sys::link_patches();
 
     let peripherals = Peripherals::take().unwrap();
-    let mut led = PinDriver::output(peripherals.pins.gpio4)?;
+    let mut internal_led = PinDriver::output(peripherals.pins.gpio2).unwrap();
 
     loop {
-        // led.set_high()?;
-        // // we are sleeping here to make sure the watchdog isn't triggered
-        // FreeRtos::delay_ms(1000);
+        internal_led.set_high().unwrap();
+        // we are sleeping here to make sure the watchdog isn't triggered
+        FreeRtos::delay_ms(500);
 
-        // led.set_low()?;
-        // FreeRtos::delay_ms(1000);
+        internal_led.set_low().unwrap();
+        FreeRtos::delay_ms(500);
     }
 }
