555db378e3c637bf70ba263e8d9c07fa3bf6e03f
HannesH <33062605+HannesGitH@users.noreply.github.com>
2023-04-03 20:56:14 +0200

    alarm done?

--- src/neopixel/effects/alarm.rs
+++ src/neopixel/effects/alarm.rs
@@ -5,7 +5,7 @@ use serde_with::{serde_as, DurationSeconds};
 
 use crate::neopixel::strip::color::Color;
 
-use super::{strobo::StroboEffect, Effect};
+use super::{hue::HueShiftEffect, solid::SolidColorEffect, strobo::StroboEffect, Effect};
 
 pub struct AlarmEffect;
 
@@ -46,16 +46,102 @@ impl Effect for AlarmEffect {
         if let Some(rt) = rt {
             if rt > config.at_s_since_1970 {
                 //TODO: play actual alarm
+
+                let seconds_to_alarm = (config.at_s_since_1970 - rt).as_secs_f32();
                 match config.alarm_type {
-                    AlarmType::Sunrise => {}
+                    AlarmType::Sunrise => {
+                        //fade in red color from 30s to 10s before alarm
+                        let red = if seconds_to_alarm > 30.0 {
+                            0.0
+                        } else if seconds_to_alarm > 10.0 {
+                            1.0 - (seconds_to_alarm - 10.0) / 20.0
+                        } else {
+                            1.0
+                        };
+                        //fade in green color from 20s to 5s before alarm
+                        let green = if seconds_to_alarm > 20.0 {
+                            0.0
+                        } else if seconds_to_alarm > 5.0 {
+                            1.0 - (seconds_to_alarm - 5.0) / 15.0
+                        } else {
+                            1.0
+                        };
+                        //fade in blue color from 10s to 0s before alarm
+                        let blue = if seconds_to_alarm > 10.0 {
+                            0.0
+                        } else {
+                            1.0 - seconds_to_alarm / 10.0
+                        };
+                        SolidColorEffect::apply(
+                            &super::solid::SolidColorConfig {
+                                color: Color::new(red, green, blue),
+                                range: config.range.clone(),
+                            },
+                            colors,
+                            dt,
+                            Some(rt),
+                        )?;
+                    }
                     AlarmType::Silvester => {
                         //countdown to 0 ; blink white every second (black in between blinks)
-                        let seconds_to_alarm = (config.at_s_since_1970 - rt).as_secs_f32();
                         if seconds_to_alarm < 0.0 {
                             //its time ; friggn party
-                            //TODO
+                            //solid base color for hue-shift to make a rainbow
+                            SolidColorEffect::apply(
+                                &super::solid::SolidColorConfig {
+                                    color: Color::red(),
+                                    range: config.range.clone(),
+                                },
+                                colors,
+                                dt,
+                                Some(rt),
+                            )?;
+                            HueShiftEffect::apply(
+                                &super::hue::HueShiftConfig {
+                                    range: config.range.clone(),
+                                    degrees_per_led: 12.0, //full rotataion in 30LEDs (1m)
+                                    degrees_per_second: 60.0, //full rotation in 6s (rather fast)
+                                },
+                                colors,
+                                dt,
+                                Some(rt),
+                            )?;
                         } else {
-                            //TODO: modulo im prinzip wie bei strobo bloß genau 1hz blinken (und bei unter 3s auch noch rot zusätzlich oder so)
+                            if seconds_to_alarm < 3.0 && seconds_to_alarm % 1.0 < 0.4 {
+                                //red afterglow in last 3s
+                                SolidColorEffect::apply(
+                                    &super::solid::SolidColorConfig {
+                                        color: Color::red(),
+                                        range: config.range.clone(),
+                                    },
+                                    colors,
+                                    dt,
+                                    Some(rt),
+                                )?;
+                            }
+                            if seconds_to_alarm % 1.0 < 0.2 {
+                                //blink white
+                                SolidColorEffect::apply(
+                                    &super::solid::SolidColorConfig {
+                                        color: Color::white(),
+                                        range: config.range.clone(),
+                                    },
+                                    colors,
+                                    dt,
+                                    Some(rt),
+                                )?;
+                            } else {
+                                //black
+                                SolidColorEffect::apply(
+                                    &super::solid::SolidColorConfig {
+                                        color: Color::black(),
+                                        range: config.range.clone(),
+                                    },
+                                    colors,
+                                    dt,
+                                    Some(rt),
+                                )?;
+                            }
                         }
                     }
                     AlarmType::Strobo => {
